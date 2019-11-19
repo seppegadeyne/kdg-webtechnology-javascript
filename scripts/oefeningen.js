8094a1d2ea5ce22a8c85dd84434c80eca8499961
Seppe Gadeyne <seppe@fushia.be>
2019-11-19 23:26:36 +0100

    Update

--- scripts/oefeningen.js
+++ scripts/oefeningen.js
@@ -1,4 +1,19 @@
-raadGetal();
+methoden();
+
+function methoden() {
+    let text;
+    text = prompt("Geef een text: ");
+    alert("charAt: " + text.charAt(1));
+    alert("slice: " + text.slice(2,10));
+    alert("toUpperCase: " + text.toUpperCase());
+    alert("blink: " + text.blink());
+    alert("legnth: " + text.length);
+    text = text.length < 3;
+    alert(text);
+    let myCounter = "1";
+    myCounter++;
+    alert(myCounter);
+}
 
 function berekenBMI() {
     let bmi, lengte, gewicht;
