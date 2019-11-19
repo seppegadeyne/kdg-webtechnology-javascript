1b35c8379fe2e79386ea848d738ca9d0e674a31e
Seppe Gadeyne <seppe@fushia.be>
2019-11-19 22:21:05 +0100

    Update

--- scripts/oefeningen.js
+++ scripts/oefeningen.js
@@ -11,10 +11,11 @@ function berekenBMI() {
 function raadGetal() {
     let getal = Math.floor(Math.random()*100) + 1;
     gevonden = false;
+    let naam = prompt("Wat is je naam? ");
     while(!gevonden) {
         let gok = prompt("Doe een gok: ");
         if(gok == getal) {
-            alert("Gevonden!");
+            alert("Gevonden, proficiat " + naam + "!");
             gevonden = true;
         } else if(gok < getal) {
             alert("Te klein!");
