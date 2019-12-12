a225a2c780fd0e4e8228d83bf825d8c82a76af12
Seppe Gadeyne <seppe@fushia.be>
2019-12-12 22:39:09 +0100

    Update

--- scripts/demoWeek3.js
+++ scripts/demoWeek3.js
@@ -59,6 +59,21 @@ function Auto(merk, model, aantalDeuren, bouwJaar) {
     this.bouwJaar = bouwJaar;
     this.prijs = prijsAuto(this.merk);
 }
+
+class NieuweAuto {
+    constructor(merk, model, aantalDeuren, bouwjaar) {
+        this.merk = merk;
+        this.model = model;
+        this.aantalDeuren = aantalDeuren;
+        this.bouwJaar = bouwjaar;
+        this.prijs = prijsAuto(this.merk);
+    }
+
+    toon() {
+        document.getElementById("demoArticle").innerHTML = "<p>" + this.merk + " " + this.model + " voor € " + this.prijs + "</p>";
+    }
+}
+
 /*
 let auto = {
     merk : "Ford",
@@ -74,6 +89,73 @@ function prijsAuto(merk) {
 }
 
 let mijnAuto = new Auto("Opel", "Corsa", 4, 2015);
-el.innerHTML = "<p>" + mijnAuto.merk + " " + mijnAuto["model"] + " voor € " + mijnAuto.prijs + "</p>";
+//el.innerHTML = "<p>" + mijnAuto.merk + " " + mijnAuto["model"] + " voor € " + mijnAuto.prijs + "</p>";
 
+let mijnTweedeAuto = new NieuweAuto("BMW", "1 reeks", 5, 2012);
+//mijnTweedeAuto.toon();
+
+// for loop for an object, use forEach for an Array
+for(let index in window.navigator) {
+    //el.innerHTML += "<p>" + index + ": " + window.navigator[index] + "</p>";
+}
+
+let familieGadeyne = ['Seija', 'Seppe', 'Senne', 'Silke', 'Luka', 'Ronny'];
+for(let index of familieGadeyne) {
+    //el.innerHTML += "<p>" + index + ": " + familieGadeyne[index] + "</p>";
+}
+
+familieGadeyne.forEach(element => {
+    //el.innerHTML += "<p>" + element + "</p>";
+});
+
+el.innerHTML = "";
+let teller = 10, getal = 10; 
+/*
+while(teller > 0) {
+    el.innerHTML += teller + "maal " + getal + " = " + teller * getal + "<br>";
+    teller--;
+}
+*/
+
+for(let i = 10; i > 0; i--) {
+    //el.innerHTML += i + "maal " + getal + " = " + i * getal + "<br>";
+}
+
+class House {
+    constructor(soort, straat, gemeente) {
+        this.soort = soort;
+        this.straat = straat;
+        this.gemeente = gemeente;
+    }
+}
+
+
+let mijnHuis = new House("Villa", "Rozenlaan", "Brecht");
+let counter = 0;
+for(let index in mijnHuis) {
+    if (counter < 10) {
+        el.innerHTML += index + " : " + mijnHuis[index] + "<br>"; 
+    } else {
+        break;
+    }
+    counter++;
+}
+
+
+let navigator = [];
+for(let index in window.navigator) {
+    navigator.push(index + " : " + window.navigator[index]);
+}
+
+console.log(navigator.sort());
+
+let i = 8;
+while(i > 0) {
+    el.innerHTML = i + "<br>";
+    i--;
+}
+
+i = 10;
+i /= 2;
+console.log(i);
 
---
+++ scripts/demoWeek5.js
@@ -0,0 +1,70 @@
+addEventListener("load", init, false);
+
+let mieren = [];
+
+function init() {
+    document.querySelector("#maak").addEventListener("click", handleMaak, false);
+    document.querySelector("#beweeg").addEventListener("click", handleBeweeg, false);
+}
+
+class Mier {
+    constructor() {
+        this.x = Math.floor(Math.random()*window.innerWidth);
+        this.y = Math.floor(Math.random()*window.innerHeight);
+        this.rotation = Math.floor(Math.random()*360);
+        this.node = document.createElement("img");
+        this.node.setAttribute("src", "../images/black_ant.png");
+        this.node.setAttribute("alt", "levende mier");
+        this.node.setAttribute("class", "levend");
+        this.node.style.position = "absolute";
+        this.updateImageTag();
+        this.node.addEventListener("click", handleVerpletter, false);
+    }
+
+    getImageTag() {
+        return this.node;
+    }
+
+    updateImageTag() {
+        this.node.style.top = this.y+"px";
+        this.node.style.left = this.x+"px";
+        this.node.style.transform = "rotate("+ this.rotation+"deg)";
+    }
+
+    beweeg() {
+        if(this.node.classList.contains("levend")) {
+            this.x += Math.floor(Math.random()*11)-5;
+            this.y += Math.floor(Math.random()*11)-5;
+            this.rotation += Math.floor(Math.random()*21)-10;
+            this.updateImageTag();
+        }
+    }
+}
+
+function handleMaak() {
+    console.log("Maak knop wordt ingedrukt");
+    let div = document.querySelector("#mieren");
+    let aantal = document.querySelector("#aantal").value;
+
+
+    for(let i = 0; i < aantal; i++) {
+        let mier = new Mier();
+        mieren.push(mier);
+        div.appendChild(mier.getImageTag());
+    }
+}
+
+function handleBeweeg() {
+    console.log("Beweeg knop wordt ingedrukt");
+    setInterval(function() {
+        for(let i = 0; i < mieren.length; i++) {
+            mieren[i].beweeg();
+        }
+    }, 100);
+}
+
+function handleVerpletter(event) {
+    event.target.setAttribute("src", "../images/ant_dead.png");
+    event.target.setAttribute("alt", "dode mier");
+    event.target.setAttribute("class", "dood");
+}
