e81e4fe07ca6ef73f678d122e275ebdd418cb4c6
Seppe Gadeyne <seppe@fushia.be>
2019-12-23 20:59:41 +0100

    Update

--- scripts/demoWeek4.js
+++ scripts/demoWeek4.js
@@ -9,10 +9,18 @@ function validateForm(event) {
 
     let fields = document.querySelectorAll(".check");
     for (field of fields) {
-        if (field.value == "") {
+        if (field.value === "") {
             document.querySelector(".error-" + field.name).innerText = field.name + " niet ingevuld!";
         } else {
             document.querySelector(".error-" + field.name).innerText = "";
+        }
+
+        if (field.name === "postcode") {
+            console.log("Value = " + field.value);
+            let regEx = /^[1-9]\d{3}$/;
+            if (!regEx.test(field.value)) {
+                document.querySelector(".error-postcode").innerText = "ongeldige postcode";
+            }
         }
     }
 }
